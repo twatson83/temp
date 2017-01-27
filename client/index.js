670715a7dcd4379d3f154e77d030ced0fb4ec248
twatson83 <tswatson@gmail.com>
2017-01-27 17:24:32 +0000

    Added fonts

--- client/index.js
+++ client/index.js
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import HomePage from './home/components/HomePage';
 
+if (process.env.BROWSER ) {
+    require ("../shared/style/fonts.scss");
+}
+
 ReactDOM.render(
     <HomePage />,
     document.getElementById('root')
